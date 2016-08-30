4f705bfa2d9533743f59b7a32afe6d40b5397ef1
Justin Cinkelj <justin.cinkelj@xlab.si>
2016-08-30 10:19:47 +0200

    ivshmem test access to memory
    Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>

--- drivers/virtio-ivshmem.cc
+++ drivers/virtio-ivshmem.cc
@@ -25,7 +25,64 @@ ivshmem::ivshmem(pci::device& pci_dev)
 {
     add_dev_status(VIRTIO_CONFIG_S_DRIVER_OK);
 
+#if 0
+    dump_config();
+    fprintf(stderr, "ivshmem pci_dev dump_config\n");
+    pci_dev.dump_config();
+    fprintf(stderr, "ivshmem pci_dev dump_config DONE\n");
 
+    pci::bar *bar;
+    bar = pci_dev.get_bar(3);
+    bar->map();
+
+    int ii;
+    size_t sz;
+    for (ii=0; ii<6; ii++) {
+        bar = pci_dev.get_bar(ii);
+        fprintf(stderr, "DBG ivshmem bar[%d]=%p\n", ii, bar);
+        if(bar) {
+            sz = bar->read_bar_size();
+            fprintf(stderr, "DBG ivshmem bar[%d] sz=%lu addr64=%p addr_mmio=%p\n", ii, sz, bar->get_addr64(), bar->get_mmio());
+        }
+    }
+    bar = pci_dev.get_bar(3);
+    sz = bar->read_bar_size();
+
+    // char pch = (char*) mmio_map(0xfe000000, sz); // Is not OK, it works only up to 8 MB large
+    volatile char* pch;
+    pch = reinterpret_cast<volatile char*>(bar->get_mmio());
+    fprintf(stderr, "DBG ivshmem DUMP pch = %p\n", pch);
+    fprintf(stderr, "DBG ivshmem DUMP-string '%s'\n", pch);
+
+    char ch;
+    if(pch) {
+        fprintf(stderr, "DBG ivshmem DUMP ");
+        for (ii=0; ii<16; ii++) {
+            ch = pch[ii];
+            ch = bar->readb(ii);
+            fprintf(stderr, "%c", ch);
+        }
+        fprintf(stderr, " dump DONE\n");
+    }
+    if (1) {
+        // will be left for next VM run
+        /*bar->writeb(0, 'A');
+        bar->writeb(1, 'B');
+        bar->writeb(2, 'C');
+        bar->writeb(3, 'D');*/
+        /*
+        *reinterpret_cast<volatile u8*>(bar->get_mmio() + 0) = 'x';
+        *reinterpret_cast<volatile u8*>(bar->get_mmio() + 1) = '1';
+        *reinterpret_cast<volatile u8*>(bar->get_mmio() + 2) = '2';
+        *reinterpret_cast<volatile u8*>(bar->get_mmio() + 3) = '3';
+        */
+        pch[0] = 'a';
+        pch[1] = 'b';
+        pch[2] = 'c';
+        pch[3] = 'd';
+    }
+    fprintf(stderr, "DBG ivshmem DUMP-string2 '%s'\n", pch);
+#endif
 }
 
 ivshmem::~ivshmem()
