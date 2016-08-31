528df674ef43ad363031d72bfd4bfaf8ab995b50
Justin Cinkelj <justin.cinkelj@xlab.si>
2016-08-31 12:22:03 +0200

    ivshmem: update test code
    Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>
    
    Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>

--- drivers/virtio-ivshmem.cc
+++ drivers/virtio-ivshmem.cc
@@ -36,6 +36,7 @@ ivshmem::ivshmem(pci::device& pci_dev)
     _size = bar->read_bar_size();
 
 #if 0
+#define OFFSET (1024*64)
     dump_config();
     fprintf(stderr, "ivshmem pci_dev dump_config\n");
     pci_dev.dump_config();
@@ -58,36 +59,36 @@ ivshmem::ivshmem(pci::device& pci_dev)
     volatile char* pch;
     pch = reinterpret_cast<volatile char*>(bar->get_mmio());
     fprintf(stderr, "DBG ivshmem DUMP pch = %p\n", pch);
-    fprintf(stderr, "DBG ivshmem DUMP-string '%s'\n", pch);
+    fprintf(stderr, "DBG ivshmem DUMP-string '%s'\n", pch+OFFSET);
 
     char ch;
     if(pch) {
         fprintf(stderr, "DBG ivshmem DUMP ");
         for (ii=0; ii<16; ii++) {
-            ch = pch[ii];
-            ch = bar->readb(ii);
+            ch = pch[ii+OFFSET];
+            ch = bar->readb(ii+OFFSET);
             fprintf(stderr, "%c", ch);
         }
         fprintf(stderr, " dump DONE\n");
     }
     if (1) {
         // will be left for next VM run
-        /*bar->writeb(0, 'A');
-        bar->writeb(1, 'B');
-        bar->writeb(2, 'C');
-        bar->writeb(3, 'D');*/
+        /*bar->writeb(OFFSET+0, 'A');
+        bar->writeb(OFFSET+1, 'B');
+        bar->writeb(OFFSET+2, 'C');
+        bar->writeb(OFFSET+3, 'D');*/
         /*
-        *reinterpret_cast<volatile u8*>(bar->get_mmio() + 0) = 'x';
-        *reinterpret_cast<volatile u8*>(bar->get_mmio() + 1) = '1';
-        *reinterpret_cast<volatile u8*>(bar->get_mmio() + 2) = '2';
-        *reinterpret_cast<volatile u8*>(bar->get_mmio() + 3) = '3';
+        *reinterpret_cast<volatile u8*>(bar->get_mmio() + OFFSET+0) = 'x';
+        *reinterpret_cast<volatile u8*>(bar->get_mmio() + OFFSET+1) = '1';
+        *reinterpret_cast<volatile u8*>(bar->get_mmio() + OFFSET+2) = '2';
+        *reinterpret_cast<volatile u8*>(bar->get_mmio() + OFFSET+3) = '3';
         */
-        pch[0] = 'a';
-        pch[1] = 'b';
-        pch[2] = 'c';
-        pch[3] = 'd';
+        pch[0+OFFSET] = 'a';
+        pch[1+OFFSET] = 'b';
+        pch[2+OFFSET] = 'c';
+        pch[3+OFFSET] = 'd';
     }
-    fprintf(stderr, "DBG ivshmem DUMP-string2 '%s'\n", pch);
+    fprintf(stderr, "DBG ivshmem DUMP-string2 '%s'\n", pch+OFFSET);
 #endif
 }
 
