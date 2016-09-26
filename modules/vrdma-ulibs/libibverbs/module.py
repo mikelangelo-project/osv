9939e9a9c142b52c851ee8df8e955c410f48110b
Shiqing Fan <shiqing.fan@huawei.com>
2016-09-26 12:29:53 +0200

    keep only the libraries that will be linked with apps, remove the links.
    Signed-off-by: Shiqing Fan <shiqing.fan@huawei.com>

--- modules/vrdma-ulibs/libibverbs/module.py
+++ modules/vrdma-ulibs/libibverbs/module.py
@@ -6,8 +6,6 @@ from osv.modules import api
 _module = '${OSV_BASE}/modules/vrdma-ulibs/libibverbs'
 
 usr_files = FileMap()
-usr_files.add(os.path.join(_module, 'libibverbs.so.1.0.0')).to('/usr/lib/libibverbs.so.1.0.0')
-usr_files.link(os.path.join(_module, '/usr/lib/libibverbs.so.1')).to('/usr/lib/libibverbs.so.1.0.0')
-usr_files.link(os.path.join(_module, '/usr/lib/libibverbs.so')).to('/usr/lib/libibverbs.so.1.0.0')
+usr_files.add(os.path.join(_module, 'libibverbs.so.1.0.0')).to('/usr/lib/libibverbs.so.1')
 usr_files.add(os.path.join(_module, 'ibv_devinfo.so')).to('/tools/ibv_devinfo.so')
 usr_files.add(os.path.join(_module, 'include/infiniband/')).to('/usr/include/infiniband/')
--- modules/vrdma-ulibs/librdmacm/module.py
+++ modules/vrdma-ulibs/librdmacm/module.py
@@ -6,9 +6,7 @@ from osv.modules import api
 _module = '${OSV_BASE}/modules/vrdma-ulibs/librdmacm'
 
 usr_files = FileMap()
-usr_files.add(os.path.join(_module, 'librdmacm.so.1.0.0')).to('/usr/lib/librdmacm.so.1.0.0')
-usr_files.link(os.path.join(_module, '/usr/lib/librdmacm.so.1')).to('/usr/lib/librdmacm.so.1.0.0')
-usr_files.link(os.path.join(_module, '/usr/lib/librdmacm.so')).to('/usr/lib/librdmacm.so.1.0.0')
+usr_files.add(os.path.join(_module, 'librdmacm.so.1.0.0')).to('/usr/lib/librdmacm.so.1')
 usr_files.add(os.path.join(_module, 'include/rdma')).to('/usr/include/rdma')
 
 default = ""
