e8f78bcc4974bc0cf2924d11f25db4facda66347
Shiqing Fan <shiqing.fan@huawei.com>
2016-10-07 15:56:18 +0200

    Update the location of the device driver library
    The mlx4 driver library is considered as a plugin
    of libibverbs, so it's better do the same as on
    Linux host and put it in /usr/lib/libibverbs/.
    
    Signed-off-by: Shiqing Fan <shiqing.fan@huawei.com>

--- modules/vrdma-ulibs/libmlx4/module.py
+++ modules/vrdma-ulibs/libmlx4/module.py
@@ -6,8 +6,8 @@ from osv.modules import api
 _module = '${OSV_BASE}/modules/vrdma-ulibs/libmlx4'
 
 usr_files = FileMap()
-usr_files.add(os.path.join(_module, 'libmlx4-rdmav2.so')).to('/usr/lib/libmlx4-rdmav2.so')
-usr_files.link(os.path.join(_module, '/usr/lib/libmlx4.so')).to('/usr/lib/libmlx4-rdmav2.so')
-usr_files.add(os.path.join(_module, 'mlx4.driver')).to('/usr/etc/libibverbs.d/mlx4.driver')
+usr_files.add(os.path.join(_module, 'libmlx4-rdmav2.so')).to('/usr/lib/libibverbs/libmlx4-rdmav2.so')
+usr_files.link(os.path.join(_module, '/usr/lib/libmlx4.so')).to('/usr/lib/libibverbs/libmlx4-rdmav2.so')
+usr_files.add(os.path.join(_module, 'mlx4.driver')).to('/etc/libibverbs.d/mlx4.driver')
 
 default = ""
